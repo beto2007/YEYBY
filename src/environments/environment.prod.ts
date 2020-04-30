2abc3fc7dda0ed3487e88818212b0e319c68ab77
rvillatoro <rvillatoro@enacment.com>
2020-04-30 02:05:42 -0500

    login pt 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: true,
   hmr: false,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'https://yeyby-c072f.web.app',
   defaultLanguage: 'es-ES',
   supportedLanguages: ['es-ES'],
 };
