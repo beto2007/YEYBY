cc703a51b30621be8553ae11a6e426a582c3d69a
rvillatoro <rvillatoro@enacment.com>
2020-05-28 03:01:19 -0500

    fixes pt 3

--- src/app/@shared/services/tools/tools.service.ts
+++ src/app/@shared/services/tools/tools.service.ts
@@ -6,6 +6,14 @@ import { ToastController } from '@ionic/angular';
   providedIn: 'root',
 })
 export class ToolsService {
+  removeAccents = (str: string) => {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
+
+  unique = (value: any, index: any, self: any) => {
+    return self.indexOf(value) === index;
+  };
+
   constructor(private toastController: ToastController) {}
 
   public dateFormatter(date: any, format: string): string {
@@ -25,5 +33,47 @@ export class ToolsService {
       ],
     });
     toast.present();
+  }
+
+  public stringSearch(str: string): string {
+    str = str.trim();
+    var noTildes: string = this.removeAccents(str);
+    const search: string = noTildes.replace(/[^a-zA-Z0-9]/g, '').toLocaleLowerCase();
+    return search;
+  }
+
+  public arraySearch(fullName: string): string[] {
+    const minLength = 3;
+    fullName = fullName.trim();
+    var noTildes: string = this.removeAccents(fullName);
+    const search: string = noTildes.replace(/[^a-zA-Z0-9\sñ]/g, '').toLocaleLowerCase();
+    let tempArray: string[] = [];
+    tempArray = tempArray.concat(fullName.split(' '));
+    tempArray = tempArray.concat(noTildes.split(' '));
+    tempArray = tempArray.concat(search.split(' '));
+    tempArray.forEach((name) => {
+      if (name.length > minLength) {
+        for (let i = name.length; i >= minLength; i--) {
+          tempArray.push(name.substring(0, i));
+        }
+      }
+    });
+    if (fullName.length > minLength) {
+      for (let i = fullName.length; i >= minLength; i--) {
+        tempArray.push(fullName.substring(0, i));
+      }
+    }
+    if (noTildes.length > minLength) {
+      for (let i = noTildes.length; i >= minLength; i--) {
+        tempArray.push(noTildes.substring(0, i));
+      }
+    }
+    if (search.length > minLength) {
+      for (let i = search.length; i >= minLength; i--) {
+        tempArray.push(search.substring(0, i));
+      }
+    }
+    tempArray = tempArray.filter(this.unique);
+    return tempArray;
   }
 }
--- src/app/deliverers/add-deliverer/add-deliverer.component.ts
+++ src/app/deliverers/add-deliverer/add-deliverer.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { DocumentReference } from '@angular/fire/firestore';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { ToolsService } from '@app/@shared/services/tools/tools.service';
 
 @Component({
   selector: 'app-add-deliverer',
@@ -22,6 +23,7 @@ export class AddDelivererComponent implements OnInit {
   imagesPath: string[] = [];
 
   constructor(
+    private tools: ToolsService,
     private modalController: ModalController,
     private formBuilder: FormBuilder,
     private afs: AngularFirestore,
@@ -118,13 +120,12 @@ export class AddDelivererComponent implements OnInit {
     const loadingOverlay = await this.loadingController.create({ message: 'Cargando' });
     loadingOverlay.present();
     try {
-      this.createSearchLabels([String(this.myForm.get('name').value), String(this.myForm.get('phone').value)]);
       let data: any = this.myForm.value;
+      let search: string[] = [];
       data.nameStr = String(data.name).toLocaleLowerCase();
-      data.search = this.createSearchLabels([
-        String(this.myForm.get('name').value),
-        String(this.myForm.get('phone').value),
-      ]);
+      search = search.concat(this.tools.arraySearch(String(this.myForm.get('name').value)));
+      search = search.concat(this.tools.arraySearch(String(this.myForm.get('phone').value)));
+      data.search = search;
       data.date = new Date();
       if (this.file) {
         data.image = await this.images();
@@ -150,11 +151,11 @@ export class AddDelivererComponent implements OnInit {
     loadingOverlay.present();
     try {
       let data: any = this.myForm.value;
+      let search: string[] = [];
       data.nameStr = String(data.name).toLocaleLowerCase();
-      data.search = this.createSearchLabels([
-        String(this.myForm.get('name').value),
-        String(this.myForm.get('phone').value),
-      ]);
+      search = search.concat(this.tools.arraySearch(String(this.myForm.get('name').value)));
+      search = search.concat(this.tools.arraySearch(String(this.myForm.get('phone').value)));
+      data.search = search;
       if (this.file) {
         data.image = await this.images();
       }
@@ -203,119 +204,6 @@ export class AddDelivererComponent implements OnInit {
     image.list.url = downloadURL3;
     image.list.path = 'deliverers/list/' + name;
     return image;
-  }
-
-  createSearchLabels(searchTerms: string[]): string[] {
-    let chars = {
-      á: 'a',
-      é: 'e',
-      í: 'i',
-      ó: 'o',
-      ú: 'u',
-      à: 'a',
-      è: 'e',
-      ì: 'i',
-      ò: 'o',
-      ù: 'u',
-      ñ: 'n',
-      Á: 'A',
-      É: 'E',
-      Í: 'I',
-      Ó: 'O',
-      Ú: 'U',
-      À: 'A',
-      È: 'E',
-      Ì: 'I',
-      Ò: 'O',
-      Ù: 'U',
-      Ñ: 'N',
-    };
-    let expr1 = /[áàéèíìóòúùñ]/gi;
-    let expr2 = /[`~!@#$%^&*()_|+\-=?;:'",.<>´\{\}\[\]\\\/]/gi;
-    let tempArray: string[] = [];
-    const unique = (value: any, index: any, self: any) => {
-      return self.indexOf(value) === index;
-    };
-    for (let i = 0; i < searchTerms.length; i++) {
-      const str = searchTerms[i];
-
-      const strToLowerCase = str.toLocaleLowerCase();
-      const strSinAcentos = str.replace(expr1, function (e) {
-        return chars[e];
-      });
-      const toLocaleLCSA = strToLowerCase.replace(expr1, function (e) {
-        return chars[e];
-      });
-      const strNoEspChar = str.replace(expr2, '');
-      const toLocaleLCEP = toLocaleLCSA.replace(expr1, function (e) {
-        return chars[e];
-      });
-      const includeAll = str
-        .toLocaleLowerCase()
-        .replace(expr1, function (e) {
-          return chars[e];
-        })
-        .replace(expr2, '');
-
-      const strNoWhite1 = str.replace(/\s/g, '');
-      const strNoWhite2 = strToLowerCase.replace(/\s/g, '');
-      const strNoWhite3 = strSinAcentos.replace(/\s/g, '');
-      const strNoWhite4 = strNoEspChar.replace(/\s/g, '');
-      const strNoWhite5 = toLocaleLCSA.replace(/\s/g, '');
-      const strNoWhite6 = toLocaleLCEP.replace(/\s/g, '');
-      const strNoWhite7 = includeAll.replace(/\s/g, '');
-
-      const array1 = str.split(' ');
-      const array2 = strToLowerCase.split(' ');
-      const array3 = strSinAcentos.split(' ');
-      const array4 = strNoEspChar.split(' ');
-      const array5 = strSinAcentos.split(' ');
-      const array6 = strNoEspChar.split(' ');
-      const array7 = includeAll.split(' ');
-
-      tempArray.push(str);
-      tempArray.push(strToLowerCase);
-      tempArray.push(strSinAcentos);
-      tempArray.push(strNoEspChar);
-      tempArray.push(toLocaleLCSA);
-      tempArray.push(toLocaleLCEP);
-      tempArray.push(includeAll);
-
-      tempArray.push(strNoWhite1);
-      tempArray.push(strNoWhite2);
-      tempArray.push(strNoWhite3);
-      tempArray.push(strNoWhite4);
-      tempArray.push(strNoWhite5);
-      tempArray.push(strNoWhite6);
-      tempArray.push(strNoWhite7);
-
-      tempArray = tempArray.concat(array1);
-      tempArray = tempArray.concat(array2);
-      tempArray = tempArray.concat(array3);
-      tempArray = tempArray.concat(array4);
-      tempArray = tempArray.concat(array5);
-      tempArray = tempArray.concat(array6);
-      tempArray = tempArray.concat(array7);
-    }
-    const minLenght: number = 5;
-    tempArray = tempArray.filter(unique);
-    let tempArray2: string[] = [];
-    for (let i = 0; i < tempArray.length; i++) {
-      const str = tempArray[i];
-      if (str.length > minLenght) {
-        for (let ii = str.length; ii >= minLenght; ii--) {
-          tempArray2.push(str.substring(0, ii));
-        }
-      }
-    }
-    tempArray = tempArray.concat(tempArray2);
-    tempArray = tempArray.filter(unique);
-    tempArray = tempArray.filter((element) => {
-      if (element.length >= minLenght) {
-        return element;
-      }
-    });
-    return tempArray;
   }
 
   save() {
